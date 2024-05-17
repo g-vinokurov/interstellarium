cf856ba7e78aada7c8db9d505d046abd82f1116c
g-vinokurov <g.vinokurov@g.nsu.ru>
2024-05-17 15:41:24 +0700

    Add user login

--- app/views.py
+++ app/views.py
@@ -2,7 +2,13 @@
 
 from flask import render_template
 from flask import send_from_directory
+from flask import redirect, url_for
 from flask import request
+
+from flask_login import login_required
+from flask_login import login_user
+from flask_login import current_user
+from flask_login import logout_user
 
 from app import app
 from app import db
@@ -10,6 +16,7 @@ from app import db
 from app import login_manager
 
 from app.models import User
+from app.forms import LoginForm
 
 
 @login_manager.user_loader
@@ -29,6 +36,41 @@ def get_robots():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'GET':
-        return render_template('login.html')
-    return ''''''
+    if current_user.is_authenticated:
+        return redirect(url_for('dashboard'))
+
+    form = LoginForm()
+    params = {'form': form, 'message': ''}
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+
+        with db.Session() as session:
+            user = session.query(User).filter(User.email == email).first()
+
+        if user is None:
+            params['message'] = 'Такого пользователя не существует'
+            return render_template('login.html', **params)
+
+        if not user.check_password(password):
+            params['message'] = 'Некорректный пароль'
+            return render_template('login.html', **params)
+
+        login_user(user, remember=True)
+        return redirect(url_for('dashboard'))
+
+    return render_template('login.html', **params)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    params = {'current_user': current_user}
+    return render_template('dashboard.html', **params)
