e3a35f9d025821ed29b0910844e9b30d473ad315
g-vinokurov <g.vinokurov@g.nsu.ru>
2024-06-11 23:57:10 +0700

    Add User Profile

--- app/users/router.py
+++ app/users/router.py
@@ -8,7 +8,10 @@ from fastapi.responses import JSONResponse
 from sqlalchemy import select
 
 from app.db import db
-from app.models import User, Department
+from app.models import User, Department, Group, Contract, Project
+from app.models import AssociationUserGroup
+from app.models import AssignmentUserProject
+from app.models import AssignmentUserContract
 
 from app.auth import get_current_user
 from app.users import schema
@@ -107,6 +110,144 @@ def api_users_create(
     return JSONResponse({'id': user_id}, status.HTTP_201_CREATED)
 
 
-@router.get('/api/users/{id}', response_model=list[schema.UserProfile])
-def api_users_get_one(id: int, current_user: User = Depends(get_current_user)):
-    pass
+@router.get('/api/users/{id}', status_code=status.HTTP_200_OK, responses={
+    200: {'model': schema.UserProfile},
+    400: {'model': schema.BadRequestError},
+    401: {'model': schema.UnauthorizedError},
+    403: {'model': schema.ForbiddenError},
+    404: {'model': schema.NotFoundError}
+})
+def api_users_get_one(
+    id: int,
+    current_user: User = Depends(get_current_user)
+):
+    query = select(
+        User.id,
+        User.name,
+        User.birthdate,
+        User.is_admin,
+        Department.id,
+        Department.name
+    )
+    query = query.join(
+        Department,
+        Department.id == User.department_id,
+        isouter=True
+    )
+    query = query.where(User.id == id)
+
+    with db.Session() as session:
+        user_data = session.execute(query).first()
+
+    if user_data is None:
+        return JSONResponse(
+            {'msg': 'item not found'}, status.HTTP_404_NOT_FOUND
+        )
+
+    user_id, user_name, user_birthdate, user_is_admin = user_data[0:4]
+    department_id, department_name = user_data[4:6]
+
+    query = select(
+        Group.id,
+        Group.name
+    )
+    query = query.join(
+        AssociationUserGroup,
+        AssociationUserGroup.user_id == User.id,
+        isouter=True
+    )
+    query = query.where(AssociationUserGroup.user_id == user_id)
+
+    with db.Session() as session:
+        groups_data = session.execute(query).all()
+
+    query = select(
+        AssignmentUserProject.id,
+        AssignmentUserProject.assignment_date,
+        AssignmentUserProject.is_assigned,
+        Project.id,
+        Project.name
+    )
+    query = query.join(
+        Project,
+        Project.id == AssignmentUserProject.project_id,
+        isouter=True
+    )
+    query = query.where(AssignmentUserProject.user_id == user_id)
+
+    with db.Session() as session:
+        projects_assignments_data = session.execute(query).all()
+
+    query = select(
+        AssignmentUserContract.id,
+        AssignmentUserContract.assignment_date,
+        AssignmentUserContract.is_assigned,
+        Contract.id,
+        Contract.name
+    )
+    query = query.join(
+        Contract,
+        Contract.id == AssignmentUserContract.contract_id,
+        isouter=True
+    )
+    query = query.where(AssignmentUserContract.user_id == user_id)
+
+    with db.Session() as session:
+        contracts_assignments_data = session.execute(query).all()
+
+    groups = []
+    for row in groups_data:
+        item = {
+            'id': row[0],
+            'name': row[1]
+        }
+        groups.append(item)
+
+    projects_assignments = []
+    for row in projects_assignments_data:
+        assignment_date = row[1]
+        if assignment_date is not None:
+            assignment_date = str(assignment_date)
+
+        item = {
+            'id': row[0],
+            'assignment_date': assignment_date,
+            'is_assigned': row[2],
+            'project': {
+                'id': row[3],
+                'name': row[4]
+            }
+        }
+        projects_assignments.append(item)
+
+    contracts_assignments = []
+    for row in contracts_assignments_data:
+        assignment_date = row[1]
+        if assignment_date is not None:
+            assignment_date = str(assignment_date)
+
+        item = {
+            'id': row[0],
+            'assignment_date': assignment_date,
+            'is_assigned': row[2],
+            'contract': {
+                'id': row[3],
+                'name': row[4]
+            }
+        }
+        contracts_assignments.append(item)
+
+    response = {
+        'id': user_id,
+        'name': user_name,
+        'birthdate': user_birthdate,
+        'is_admin': user_is_admin,
+        'department': {
+            'id': department_id,
+            'name': department_name
+        },
+        'groups': groups,
+        'projects_assignments': projects_assignments,
+        'contracts_assignments': contracts_assignments
+    }
+    return JSONResponse(response, status.HTTP_200_OK)
--- app/users/schema.py
+++ app/users/schema.py
@@ -12,6 +12,35 @@ from app.schema import NotFoundError
 from app.schema import CreatedResponse
 
 
+class Group(BaseModel):
+    id: Optional[int] = None
+    name: Optional[str] = None
+
+
+class Project(BaseModel):
+    id: Optional[int] = None
+    name: Optional[int] = None
+
+
+class Contract(BaseModel):
+    id: Optional[int] = None
+    name: Optional[int] = None
+
+
+class ProjectAssignment(BaseModel):
+    id: Optional[int] = None
+    assignment_date: Optional[str] = None
+    is_assigned: Optional[bool] = None
+    project: Project
+
+
+class ContractAssignment(BaseModel):
+    id: Optional[int] = None
+    assignment_date: Optional[str] = None
+    is_assigned: Optional[bool] = None
+    contract: Contract
+
+
 class Department(BaseModel):
     id: Optional[int] = None
     name: Optional[str] = None
@@ -32,4 +61,11 @@ class CreateUserRequest(BaseModel):
 
 
 class UserProfile(BaseModel):
-    pass
+    id: int
+    name: Optional[str] = None
+    birthdate: Optional[str] = None
+    is_admin: Optional[bool] = None
+    department: Department
+    groups: list[Group]
+    projects_assignments: list[ProjectAssignment]
+    contracts_assignments: list[ContractAssignment]
