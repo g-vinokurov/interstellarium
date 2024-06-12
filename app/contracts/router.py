34c11c2ef98e7c27279ecceb65ab1a53bcb6e9b1
g-vinokurov <g.vinokurov@g.nsu.ru>
2024-06-13 03:01:39 +0700

    Add Contract Chief and Group Updating

--- app/contracts/router.py
+++ app/contracts/router.py
@@ -245,3 +245,75 @@ def api_contracts_get_one(
     }
 
     return JSONResponse(response, status.HTTP_200_OK)
+
+
+@router.put('/api/contracts/{id}/chief', status_code=status.HTTP_200_OK, responses={
+    200: {'model': schema.OkResponse},
+    400: {'model': schema.BadRequestError},
+    401: {'model': schema.UnauthorizedError},
+    403: {'model': schema.ForbiddenError},
+    404: {'model': schema.NotFoundError}
+})
+def api_contracts_update_chief(
+    id: int,
+    request: schema.UserID,
+    current_user: User = Depends(get_current_user)
+):
+    if not current_user.is_admin and not current_user.is_superuser:
+        return JSONResponse(
+            {'msg': 'access denied'}, status.HTTP_403_FORBIDDEN
+        )
+
+    session = db.Session()
+
+    contract = session.query(Contract).get(id)
+    chief = session.query(User).get(request.id)
+
+    if contract is None:
+        return JSONResponse(
+            {'msg': 'item not found'}, status.HTTP_404_NOT_FOUND
+        )
+
+    if chief is None:
+        contract.chief_id = None
+    else:
+        contract.chief_id = chief.id
+
+    session.commit()
+    return JSONResponse({'msg': 'ok'}, status.HTTP_200_OK)
+
+
+@router.put('/api/contracts/{id}/group', status_code=status.HTTP_200_OK, responses={
+    200: {'model': schema.OkResponse},
+    400: {'model': schema.BadRequestError},
+    401: {'model': schema.UnauthorizedError},
+    403: {'model': schema.ForbiddenError},
+    404: {'model': schema.NotFoundError}
+})
+def api_contracts_update_group(
+    id: int,
+    request: schema.GroupID,
+    current_user: User = Depends(get_current_user)
+):
+    if not current_user.is_admin and not current_user.is_superuser:
+        return JSONResponse(
+            {'msg': 'access denied'}, status.HTTP_403_FORBIDDEN
+        )
+
+    session = db.Session()
+
+    contract = session.query(Contract).get(id)
+    group = session.query(Group).get(request.id)
+
+    if contract is None:
+        return JSONResponse(
+            {'msg': 'item not found'}, status.HTTP_404_NOT_FOUND
+        )
+
+    if group is None:
+        contract.group_id = None
+    else:
+        contract.group_id = group.id
+
+    session.commit()
+    return JSONResponse({'msg': 'ok'}, status.HTTP_200_OK)
